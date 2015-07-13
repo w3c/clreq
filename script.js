2fc257aa4e22227407f67b6d52615d814d657357
r12a <ishida@w3.org>
2015-07-13 17:01:38 +0100

    added alert to reload page before switching a second time

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
+var switched = false;
+
 function switch2zh () {
+	if (switched) { alert('Refresh the page, then click on this button again.'); return; }
+	
 	var en = document.querySelectorAll('[data-lang=en]')
 	for (var i=0;i<en.length;i++) en[i].style.display='none' 
 	document.getElementById('languageStyling').textContent=''
 	document.documentElement.lang = 'zh'
+	switched = true;
 	}
 	
 	
 function switch2en () {
+	if (switched) { alert('Refresh the page, then click on this button again.'); return; }
+	
 	var zh = document.querySelectorAll('[data-lang=zh]')
 	for (i=0;i<zh.length;i++) zh[i].style.display='none' 
 	document.getElementById('languageStyling').textContent=''
 	document.documentElement.lang = 'en'
+	switched = true;
 	}

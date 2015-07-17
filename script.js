11e679f556b4b38c773a08dea6c7637f6656007a
r12a <ishida@w3.org>
2015-07-17 16:38:24 +0100

    + addLangAttrs () adds lang attributes wherever there is a data-lang attribute except that, if there's already a lang attribute in the tag, that tag is skipped this is done by js to reduce the burden on editors note that this may still produce temporarily incorrect labelling where text is awaiting translation

--- script.js
+++ script.js
@@ -15,8 +15,19 @@ function switch2en () {
 	if (switched) { alert('Refresh the page, then click on this button again.'); return; }
 	
 	var zh = document.querySelectorAll('[data-lang=zh]')
-	for (i=0;i<zh.length;i++) zh[i].style.display='none' 
+	for (var i=0;i<zh.length;i++) zh[i].style.display='none' 
 	document.getElementById('languageStyling').textContent=''
 	document.documentElement.lang = 'en'
 	switched = true;
+	}
+
+
+function addLangAttrs () {
+	// adds lang attributes wherever there is a data-lang attribute
+	// this is done by js to reduce burden on editors
+	// if there's already a lang attribute in the tag, that tag is skipped
+	// note that this may still produce temporarily incorrect labelling where text is awaiting translation
+	
+	var zh = document.querySelectorAll('[data-lang=zh]')
+	for (i=0;i<zh.length;i++) { if (zh[i].lang == '') { zh[i].lang='zh'} }
 	}
